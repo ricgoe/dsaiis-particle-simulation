7d01f9478521c6bdc1be01fc0a8b75d7793670ae
ricgoe <bluemchen_fenchel_0d@icloud.com>
2024-11-14 14:44:43 +0100

    created particle system as wrapper

---
+++ part_wrapper.py
@@ -0,0 +1,64 @@
+import numpy as np
+from particle import Particle
+from matplotlib import pyplot as plt
+
+class ParticleSystem:
+    def __init__(self, width: int, height: int, color_distribution: list[tuple[tuple[int, int, int], int]], step_size: float = 5):
+        self.color_distribution = color_distribution
+        self.width: int = width
+        self.height: int = height
+        self._particles: list[Particle] = self.init_particles()
+        self.step_size: float = step_size
+    
+    @property
+    def particles(self):
+        return self._particles
+    
+    def init_particles(self):
+        tmp = []
+        for rgb, num in self.color_distribution:
+            __x = np.random.uniform(0, self.width, num)
+            __y = np.random.uniform(0, self.height, num)
+            for x, y in zip(__x, __y):
+                tmp.append(Particle(color=rgb, x_pos=x, y_pos=y))
+        return tmp
+                
+    def move_particles(self):
+        for particle in self.particles:
+            step = np.random.normal(0, self.step_size, 2)
+            new_x = np.mod(particle.x_pos + step[0], self.width)
+            new_y = np.mod(particle.y_pos + step[1], self.height)
+            particle.move(new_x, new_y)
+
+    
+    
+    
+    def plot_particles(self):
+        self.init_particles()
+        plt.ion()
+        fig, ax = plt.subplots()
+        ax.set_xlim(0, self.width)
+        ax.set_ylim(0, self.height)
+
+        while True:
+            ax.clear()
+            ax.set_xlim(0, self.width)
+            ax.set_ylim(0, self.height)
+            
+            
+            for particle in self.particles:
+                col = (particle.color[0]/255, particle.color[1]/255, particle.color[2]/255)
+                ax.scatter(particle.x_pos, particle.y_pos, color=col)
+                
+            plt.pause(0.05) 
+            self.move_particles()
+
+            
+    
+
+            
+
+if __name__ == "__main__":
+    particle_system = ParticleSystem(width=1000, height=1000, color_distribution=[((0, 255, 0), 25), ((255, 0, 0), 25), ((0, 0, 255), 25)])
+    particle_system.plot_particles()
+    
