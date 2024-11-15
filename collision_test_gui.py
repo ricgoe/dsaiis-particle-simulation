847f8785491a62e055a288dcdee9d7bc8e905f4b
YolloPlays <spexe0011@gmail.com>
2024-11-15 02:53:51 +0100

    big improvements. nice color handling done. TODO: More interactive... add more sliders dynamically

--- collision_test_gui.py
+++ collision_test_gui.py
@@ -1,11 +1,11 @@
 from vispy import app, scene
 import numpy as np
 from vispy.app import Timer
-from particle_system import ParticleSystem
+from collision_test import test_ParticleSystem
 
 
 
-part_sys = ParticleSystem(width=10000, height=10000, color_distribution=[((1, 0, 0, 1), 100000), ((0, 1, 0, 1), 100000)], step_size=.5, radius=.5)
+part_sys = test_ParticleSystem()
 
 canvas = scene.SceneCanvas(keys="interactive", show=True)
 view = canvas.central_widget.add_view()
@@ -23,6 +23,7 @@ view.add(scatter)
 def update(event):  
     part_sys.move_particles()
     x, y = part_sys.particles[:,0], part_sys.particles[:,1]
+    print(x, y)
     scatter.set_data(np.array([x,y]).T, edge_color=part_sys.particles[:,2:], face_color=part_sys.particles[:,2:], size=part_sys.radius)
     
 timer = Timer(interval=0.01, connect=update, start=True)
