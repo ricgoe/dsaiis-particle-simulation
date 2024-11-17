9cb29a679aa985f7f5f341cfa41743045c624edc
joha0495 <jochen.hartlieb@study.hs-duesseldorf.de>
2024-11-17 20:54:37 +0100

    comments added

--- part_wrapper.py
+++ part_wrapper.py
@@ -26,39 +26,46 @@ class ParticleSystem:
                 
     def move_particles(self):
        for particle in self.particles:
+            
+            # normaler step wie vorher, *0.2 , weil sonst zu viel anteil an randomness. 0.2 erstaml zufällig gewählt
             step = np.random.normal(0, self.step_size, 2)
             new_x = (particle.x_pos + 0.2 * step[0])
             new_y = (particle.y_pos + 0.2 * step[1])
 
+            # anziehung und abstoßung
             repulsion_x, repulsion_y = 0, 0
             attraction_x, attraction_y = 0, 0
             for other in self.particles:
 
+                # distanz zu jedem anderen particle
                 x_distance = particle.x_pos - other.x_pos
                 y_distance = particle.y_pos - other.y_pos
                 distance = np.sqrt(x_distance**2 + y_distance**2)
 
-                if distance > 1e-6:
-                
+
+                if distance > 0:
+                    
+                    # bei unterschiedlicher farbe: abstoßung und wenn in der distanz liegt, die beeinflusst werden soll
                     if particle.color != other.color and distance < self.min_distance:
 
                     
-
+                        # force ist min.distance - distance, damit größer, wenn abstand kleiner
                         force = (self.min_distance - distance) 
+                        # columbsche abstand mal die berechnete force geteilte durch erstmal random parameter, der gut gepasst hat
                         repulsion_x += (x_distance/distance) * force / (self.min_distance / 2)
                         repulsion_y += (y_distance/distance) * force / (self.min_distance / 2)
 
                         
-
+                    # bei gleicher farbe: anziehung
                     elif particle.color == other.color and distance < self.min_distance:
 
-
+                        
                         force = (self.min_distance - distance) 
                         attraction_x += (x_distance/distance) * force / (self.min_distance / 2)
                         attraction_y += (y_distance/distance) * force / (self.min_distance / 2)
 
                         
-
+            # anziehung das gegenteil von abstoßung, deswegen anziehung abgezogen und abstoßung addiert zu neuen koordinaten
             new_x = np.mod(new_x + repulsion_x - attraction_x, self.width)
             new_y = np.mod(new_y + repulsion_y - attraction_y, self.height)
 
