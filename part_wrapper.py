264c2e52158a564a7b951d696bd06eb3e45b8f60
Jochen Hartlieb <jochen.hartlieb@study.hs-duesseldorf.de>
2024-12-17 22:02:53 +0100

    first try to implement numba

--- part_wrapper.py
+++ part_wrapper.py
@@ -1,11 +1,12 @@
 import numpy as np
 from particle import Particle
 from scipy.spatial import KDTree
-from numba import njit
+import numba
+from numba.typed import List
 
 class ParticleSystem:
 
-    def __init__(self, width: int, height: int, color_distribution: list[tuple[tuple[int, int, int], int]], step_size: float = 2, radius: int = 10, min_distance: float = 200):
+    def __init__(self, width: int, height: int, color_distribution: list[tuple[tuple[int, int, int], int]], step_size: float = 2, radius: int = 10, min_distance: float = 300):
 
 
         self.color_distribution = color_distribution
@@ -28,54 +29,92 @@ class ParticleSystem:
                 tmp.append(Particle(color=rgba, x_pos=x, y_pos=y))
         return tmp
                 
-
     def move_particles(self):
-       positions = np.array([[particle.x_pos, particle.y_pos] for particle in self.particles])
+        positions = np.array([(p.x_pos, p.y_pos) for p in self.particles])
+        colors = np.array([p.color for p in self.particles])
 
-       
+        neighbors = self.check_neighbors(positions = positions) 
+        # neighbors = np.array(neighbors)
 
-       tree = KDTree(positions)
+        new_positions = move_particles_numba(positions, colors,self.step_size, self.min_distance, self.width, self.height, neighbors)
 
-       for particle in (self.particles):
+        for idx, particle in enumerate(self.particles):
+            particle.move(new_positions[idx, 0], new_positions[idx, 1])
             
-            if particle.x_pos < 0 or particle.x_pos > self.width or particle.y_pos < 0 or particle.y_pos > self.height:
-                raise Exception("particle out of bounds")
-            
-            # normaler step wie vorher, *0.2 , weil sonst zu viel anteil an randomness. 0.2 erstaml zufällig gewählt
-            step = np.random.normal(0, self.step_size, 2)
-            new_x = (particle.x_pos + 0.2 * step[0])
-            new_y = (particle.y_pos + 0.2 * step[1])
+    def check_edge_neighbors(self, particle, tree):
+        edge_particles = []
 
-            # anziehung und abstoßung3
+        if particle[0] < self.width + self.min_distance:
+            mirrored_particles = tree.query_ball_point((particle[0] + self.width, particle[1]), r=self.min_distance)
+            edge_particles.extend(mirrored_particles)
+
+        elif particle[0] > self.width - self.min_distance:
+            mirrored_particles = tree.query_ball_point((particle[0] - self.width, particle[1]), r=self.min_distance)
+            edge_particles.extend(mirrored_particles)
+
+        if particle[1] < self.height + self.min_distance:
+            mirrored_particles = tree.query_ball_point((particle[0], particle[1] + self.height), r=self.min_distance)
+            edge_particles.extend(mirrored_particles)
+
+        elif particle[1] > self.height - self.min_distance:
+            mirrored_particles = tree.query_ball_point((particle[0], particle[1] - self.height), r=self.min_distance)
+            edge_particles.extend(mirrored_particles)
+
+        return edge_particles
+        
+    def check_neighbors(self, positions):
+        
+        tree = KDTree(positions)
+
+        neighbors = List()
+
+        for particle in positions:
+
+            particle_neighbors = tree.query_ball_point((particle[0], particle[1]), r=self.min_distance)
+            particle_neighbors.extend(self.check_edge_neighbors(particle, tree))
+
+            neighbors.append(List(particle_neighbors))
+                
+        return neighbors
+        
+@numba.njit #tenk
+def move_particles_numba(positions, colors, step_size, min_distance, width, height, neighbors):	
+        new_positions = np.copy(positions)
+        num_particles = len(positions)
+
+        for particle in range(num_particles):
+
+            step = np.random.normal(0, step_size, 2)
+            new_x = (positions[particle, 0] + 0.2 * step[0])
+            new_y = (positions[particle, 1] + 0.2 * step[1])
+
             repulsion_x, repulsion_y = 0, 0
             attraction_x, attraction_y = 0, 0
 
-            # alle Partikel, die im radius min_distance liegen
-            neighbors = tree.query_ball_point((particle.x_pos, particle.y_pos), r=self.min_distance)
-            edge_neighbors = self.check_edge_neighbors(particle, tree)
-            neighbors.extend(edge_neighbors)
+            
 
-            for neighbor in neighbors:
+            for j in neighbors[particle]:
 
-                other = self.particles[neighbor]
+                # if positions[particle, 0] == positions[j, 0] and positions[particle, 1] == positions[j, 1]:
+                #     continue
 
                 # distanz zu jedem anderen particle
-                x_distance = particle.x_pos - other.x_pos
-                y_distance = particle.y_pos - other.y_pos
+                x_distance = positions[particle, 0] - positions[j,0]
+                y_distance = positions[particle, 1] - positions[j, 1]
 
             
-                if x_distance > self.min_distance:
-                    x_distance = 0 - (self.width - x_distance)
+                if x_distance > min_distance:
+                    x_distance = 0 - (width - x_distance)
                     
-                if y_distance > self.min_distance:
-                    y_distance = 0 - (self.height - y_distance)
+                if y_distance > min_distance:
+                    y_distance = 0 - (height - y_distance)
                     
 
-                if x_distance < -self.min_distance:
-                    x_distance = self.width - abs(x_distance)
+                if x_distance < -min_distance:
+                    x_distance = width - abs(x_distance)
 
-                if y_distance < -self.min_distance:
-                    y_distance = self.height - abs(y_distance)
+                if y_distance < -min_distance:
+                    y_distance = height - abs(y_distance)
 
 
                     
@@ -83,64 +122,37 @@ class ParticleSystem:
                 distance = np.sqrt(x_distance**2 + y_distance**2)
 
 
-                if distance < self.min_distance and distance > 0: #???
+                if distance < min_distance and distance > 0: #???
                     
                     # bei unterschiedlicher farbe: abstoßung und wenn in der distanz liegt, die beeinflusst werden soll
-                    if particle.color != other.color:
+                    if colors[particle][0] != colors[j][0] or colors[particle][1] != colors[j][1] or colors[particle][2] != colors[j][2]:
 
                     
                         # force ist min.distance - distance, damit größer, wenn abstand kleiner
-                        force = (self.min_distance - distance) 
+                        # force = (self.min_distance - distance) 
                         # columbsche abstand mal die berechnete force geteilte durch erstmal random parameter, der gut gepasst hat
                         repulsion_x += (x_distance/distance) #* force / (self.min_distance / 2)
                         repulsion_y += (y_distance/distance) #* force / (self.min_distance / 2)
 
                         
                     # bei gleicher farbe: anziehung
-                    elif particle.color == other.color:
+                    elif colors[particle][0] == colors[j][0] and colors[particle][1] == colors[j][1] and colors[particle][2] == colors[j][2]:
 
                         
-                        force = (self.min_distance - distance) 
+                        'force = (self.min_distance - distance)' 
                         attraction_x += (x_distance/distance)  #* force / (self.min_distance / 2)
                         attraction_y += (y_distance/distance) #* force / (self.min_distance / 2)
-
-                        
-            # anziehung das gegenteil von abstoßung, deswegen anziehung abgezogen und abstoßung addiert zu neuen koordinaten
+            
             force_x = (repulsion_x - attraction_x)
             force_y = (repulsion_y - attraction_y)
-            new_x = np.mod(new_x + force_x, self.width)
-            new_y = np.mod(new_y + force_y, self.height)
-            with open ("log.txt", "w") as f:
-                f.write(f"{new_x}, {new_y}\n")
+            new_x = np.mod(new_x + force_x, width)
+            new_y = np.mod(new_y + force_y, height)
 
-            particle.move(new_x, new_y)
+            new_positions[particle][0] = new_x
+            new_positions[particle][1] = new_y
             
-    def check_edge_neighbors(self, particle, tree):
-        edge_particles = []
 
-        if particle.x_pos < self.width + self.min_distance:
-            mirrored_particles = tree.query_ball_point((particle.x_pos + self.width, particle.y_pos), r=self.min_distance)
-            edge_particles.extend(mirrored_particles)
+        return new_positions
 
-        elif particle.x_pos > self.width - self.min_distance:
-            mirrored_particles = tree.query_ball_point((particle.x_pos - self.width, particle.y_pos), r=self.min_distance)
-            edge_particles.extend(mirrored_particles)
-
-        if particle.y_pos < self.height + self.min_distance:
-            mirrored_particles = tree.query_ball_point((particle.x_pos, particle.y_pos + self.height), r=self.min_distance)
-            edge_particles.extend(mirrored_particles)
-
-        elif particle.y_pos > self.height - self.min_distance:
-            mirrored_particles = tree.query_ball_point((particle.x_pos, particle.y_pos - self.height), r=self.min_distance)
-            edge_particles.extend(mirrored_particles)
-
-        return edge_particles
-        
-
-
-            
-if __name__ == "__main__":
-
-    particle_system = ParticleSystem(width=1000, height=1000, color_distribution=[((255, 0, 0), 100)], step_size= 10)
 
     
