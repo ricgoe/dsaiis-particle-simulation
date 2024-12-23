115fae962786f8d3c06857d2acba5be5e78c8a0b
YolloPlays <spexe0011@gmail.com>
2024-12-23 15:08:38 +0100

    finalized changes. added necessary btns, including reset and save. made stub for rel and n_particles

--- GUI.py
+++ GUI.py
@@ -3,6 +3,8 @@ QPushButton, QHBoxLayout, QSlider, QLabel, QSizePolicy, QSpacerItem)
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QColor
 import re
+from matplotlib.pyplot import get_cmap
+from vispy_backend_stub import Canvas
 
 
 from custom_widgets.widgets import PopupWidget, CustomColorPicker, SquareWidget
@@ -13,16 +15,18 @@ TICK_RATE = 20
 CIRCLE_SION = 2
 BORDER = 40
 NPARTICLES = 5
-RELATIONSHIPS = NPARTICLES*2
+RELATIONSHIPS = 10
 MAX_PARTICLES = 750
-COLOR_MAP = "inferno"
+COLOR_MAP = "viridis"
 STEP_SIZE = 3          
                 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         
+        self.cmap = get_cmap(COLOR_MAP, RELATIONSHIPS+1)
         self.color_distrubution = {}
+        self.relationships = {}
         
         self.setWindowTitle("Basic PySide6 GUI")
         self.setGeometry(100, 100, 1500, 800)
@@ -52,11 +56,29 @@ class MainWindow(QMainWindow):
         
         rel_matrix = SquareWidget()
         # rel_matrix.setStyleSheet("background-color: #c29233;")
-        self.rel_layout = QGridLayout(rel_matrix, )
-        self.rel_layout.setContentsMargins(1,1,1,1)
-        self.rel_layout.setSpacing(1)
+        self.rel_layout = QGridLayout(rel_matrix)
+        self.rel_layout.setSpacing(3)
         ctrl_layout.addWidget(rel_matrix)
+        
         ctrl_layout.addSpacerItem(QSpacerItem(0, 0, QSizePolicy.Expanding, QSizePolicy.Expanding))
+        
+        self.save_btn = QPushButton("Save", styleSheet="color: white; font-size: 20px; background-color: #31313a;")
+        self.save_btn.hide()
+        self.save_btn.clicked.connect(self.saved)
+        ctrl_layout.addWidget(self.save_btn)
+        
+        self.reset_btn = QPushButton("Reset", styleSheet="color: white; font-size: 20px; background-color: #31313a;")
+        self.reset_btn.hide()
+        self.reset_btn.clicked.connect(self.reset)
+        ctrl_layout.addWidget(self.reset_btn)
+        
+        
+        canvas = Canvas() # EMIL
+        self.canvas_layout.addWidget(canvas.native)
+        
+        self.show()
+        
+        
     
     def add_particle_color(self):
         if len(self.color_distrubution) < NPARTICLES:
@@ -70,7 +92,7 @@ class MainWindow(QMainWindow):
             color_box1.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
             color_box2.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
             self.color_distrubution[btn] = [(255, 0, 0, 255), int(MAX_PARTICLES//2)]
-            btn.clicked.connect(lambda: self.show_color_picker(btn, (color_box1, color_box2)))
+            btn.clicked.connect(lambda: self.show_color_picker(btn, [color_box1, color_box2]))
             _c_layout.addWidget(btn, 1)
             n = len(self.color_distrubution)
             self.rel_layout.addWidget(color_box1, n, 0)
@@ -80,37 +102,89 @@ class MainWindow(QMainWindow):
             slider = QSlider(Qt.Horizontal)
             slider.setRange(1, MAX_PARTICLES)
             slider.setValue(MAX_PARTICLES//2)
-            slider.valueChanged.connect(lambda val: self.slider_changed(val, label, btn))
+            slider.valueChanged.connect(lambda val: self.n_particle_slider_changed(val, label, btn))
             _c_layout.addWidget(slider, SPLIT+1)
             _c_layout.addWidget(label, 1)
             for i in range(1, n+1):
                 for j in range(1, n+1):
                     if i == n or j == n:
-                        rel_btn = QPushButton(styleSheet="background-color: #36a840;")
+                        rel_btn = QPushButton(styleSheet=f"background-color: {self.get_cmap_color(0)}; margin: 0; padding: 0; border-radius: 0;")
                         rel_btn.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+                        rel_btn.clicked.connect(lambda _, btn=rel_btn, i=i, j=j: self.show_relationship_slider(btn, i, j))
                         self.rel_layout.addWidget(rel_btn, i, j)
-            if len(self.color_distrubution) >= NPARTICLES:
+                        self.relationships[(i, j)] = 0
+            if len(self.color_distrubution) == 1:
+                self.save_btn.show()
+                self.reset_btn.show()
+            elif len(self.color_distrubution) >= NPARTICLES:
                 self.adder_btn.hide()
                         
-            
-    def show_color_picker(self, btn: QPushButton, boxes=tuple[QWidget]):
+    
+    def show_relationship_slider(self, btn: QPushButton, i: int, j: int):
+        pop = PopupWidget(self)
+        pop.setStyleSheet("background-color: #31313a;")
+        lyt = QVBoxLayout(pop)
+        label = QLabel(str(self.relationships[(i, j)]), styleSheet="color: white;", alignment=Qt.AlignCenter)
+        slider = QSlider(Qt.Horizontal)
+        slider.setRange(-int(RELATIONSHIPS/2), int(RELATIONSHIPS/2))
+        slider.setValue(self.relationships[(i, j)])
+        slider.valueChanged.connect(lambda val: self.relationship_slider_changed(val, btn, label, i, j))
+        lyt.addWidget(slider)
+        lyt.addWidget(label)
+        pop.show()
+        
+    def relationship_slider_changed(self, val, btn: QPushButton, label: QLabel, i: int, j: int):
+        tmp_css = re.sub(r"background-color:\s*([^;]+)", f"background-color: {self.get_cmap_color(val)};", btn.styleSheet())
+        btn.setStyleSheet(tmp_css)
+        label.setText(str(val))
+        self.relationships[(i, j)] = val
+        
+        
+          
+    def show_color_picker(self, btn: QPushButton, boxes=list[QWidget]):
         pop = PopupWidget(self)
         pop.setStyleSheet("background-color: #31313a;")
         lyt = QVBoxLayout(pop)
         cp = CustomColorPicker()
         _hex = re.match(r"background-color:\s*([^;]+)", btn.styleSheet()).group(1)
         cp.setCurrentColor(_hex)
-        cp.currentColorChanged.connect(lambda color: self.color_changed(color, btn))
+        cp.currentColorChanged.connect(lambda color: self.color_changed(color, btn, boxes))
         lyt.addWidget(cp)
         pop.show()
         
-    def color_changed(self, color: QColor, btn: QPushButton, boxes=tuple[QWidget]):
+    def color_changed(self, color: QColor, btn: QPushButton, boxes=list[QWidget]):
         btn.setStyleSheet(f"background-color: {color.name()};")
+        for i in boxes: i.setStyleSheet(f"background-color: {color.name()};")
         self.color_distrubution[btn][0] = color.toTuple()
         
-    def slider_changed(self, val, label: QLabel, btn: QPushButton):
+        
+    def n_particle_slider_changed(self, val, label: QLabel, btn: QPushButton):
         label.setText(str(val))
         self.color_distrubution[btn][1] = val
+
+    def get_cmap_color(self, idx):
+        return QColor.fromRgbF(*self.cmap(int(RELATIONSHIPS/2)+idx)).name()
+    
+    def clear_layout(self, layout):
+        while layout.count():  # While the layout has widgets
+            child = layout.takeAt(0)  # Take the first item in the layout
+            if child.widget():  # If it's a widget (not a nested layout or spacer)
+                child.widget().deleteLater()  # Mark the widget for deletion
+            elif child.layout():  # If it's a nested layout
+                self.clear_layout(child.layout())  # Recursively clear the nested layout
+    
+    def saved(self):
+        _c_map = list(self.color_distrubution.values())
+        print(_c_map)
+        print(self.relationships)
+        
+    def reset(self):
+        self.color_distrubution = {}
+        self.relationships = {}
+        self.clear_layout(self.particle_layout)
+        self.clear_layout(self.rel_layout)
+        self.adder_btn.show()
+
         
 
 if __name__ == "__main__":
