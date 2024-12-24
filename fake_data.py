31e044882962f5fda8f9ed838281fc2082b9a01f
Emil Ötting <emil.oetting@gmx.de>
2024-12-24 03:06:07 +0100

    Added particle sizes, increased amount of particles

--- fake_data.py
+++ fake_data.py
@@ -1,9 +1,15 @@
 import numpy as np
 
-n_points = 100
+n_points = 1000
 positions = np.random.rand(n_points, 2) * 10  # 2D array mit n_points vielen zufälligen Punkten, *10 um die Punkte auf dem Bildschirm zu verteilen
 colors = np.ones((n_points, 4))  # 4 weil rgba, letzter wert ist alpha, machen wir mal immer 1
 colors[:, :3] = np.random.rand(n_points, 3)  # rgb werte zufällig generieren
 
+#ganzzahlige Partikelgrößen erzeugen
+particle_size = np.random.randint(low=10, high=25,size=(n_points,)) # braucht INTs da Werte Pixelanzahl beschreiben
+
+#particle_size = np.ones((n_points,)) * 10
+
 POSITIONS = positions
 COLORS = colors
+PARTICLE_SIZE = particle_size
