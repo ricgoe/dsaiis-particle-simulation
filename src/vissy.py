8c03f1a1fa77cf6db6986371af103ebbc7224bba
ricgoe <bluemchen_fenchel_0d@icloud.com>
2025-02-10 19:16:51 +0100

    added support for interaction

--- src/vissy.py
+++ src/vissy.py
@@ -11,24 +11,33 @@ class DummyVisualizer:
         self.gui_sim_ratio = gui_sim_ratio
         self.sim_width = self.canvas_width/self.gui_sim_ratio
         self.sim_height = self.canvas_height/self.gui_sim_ratio
-        self.sim_radius = 5
+        self.sim_radius = 10
         self.fps = fps
         self.interval = 1/self.fps
+        interaction_matrix = np.array([[1, 0, 0], 
+                                       [0, 1, 0], 
+                                       [0, 0, 1]])
+        #interaction_matrix = np.random.uniform(-1, 1, (3, 3))
+        #interaction_matrix = (interaction_matrix + interaction_matrix.T) / 2
         
         self.part_sys = ParticleSystem(
             width=self.sim_width,
             height=self.sim_height,
             color_distribution=[
-                ((1, 0, 0), int(self.num_parts/2)),  
-                ((0, 0, 1), int(self.num_parts/2))
+                ((1, 0, 0), int(self.num_parts/3)),  
+                ((0, 1, 0), int(self.num_parts/3)),
+                ((0, 0, 1), int(self.num_parts/3)),
             ],
             radius=self.sim_radius,
-            delta_t= self.interval
+            delta_t= self.interval,
+            interaction_matrix = interaction_matrix
         )
 
+        print(interaction_matrix)
         # mapping from color indices to RGBA (workaround)
         self.color_map = np.array([
             [1, 0, 0, 1],
+            [0, 0, 1, 1],
             [0, 1, 0, 1]
         ])
 
@@ -73,18 +82,22 @@ class DummyVisualizer:
         """
         Update function to move particles and update scatter data.
         """
-        self.part_sys.move_particles()
-        x, y = self.part_sys.particles[:, 0], self.part_sys.particles[:, 1]
-        self.scatter.set_data(
-            np.column_stack((x, y)),
-            edge_color=self.particle_colors,
-            face_color=self.particle_colors,
-            size=self.pixel_radius
-        )
+        try:
+            self.part_sys.move_particles(interaction_radius=60)
+            x, y = self.part_sys.particles[:, 0], self.part_sys.particles[:, 1]
+            self.scatter.set_data(
+                np.column_stack((x, y)),
+                edge_color=self.particle_colors,
+                face_color=self.particle_colors,
+                size=self.pixel_radius
+            )
+        except KeyboardInterrupt:
+            with open('max_vel.txt', 'w') as f:
+                f.write(str(self.part_sys.max_vel))
 
 
 if __name__ == "__main__":
-    vissy = DummyVisualizer(canvas_width=1000, canvas_height=1000, num_parts=5000, fps=30, gui_sim_ratio = 1)
+    vissy = DummyVisualizer(canvas_width=1000, canvas_height=1000, num_parts=1500, fps=30, gui_sim_ratio = 1)
     vissy.create_canvas()
     timer = Timer(vissy.interval, connect=vissy.update, start=True)
     app.run()
