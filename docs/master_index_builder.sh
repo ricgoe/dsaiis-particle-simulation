bcc54e3c5b4bdd48e6c824aba49f36842e29e35b
Emil Ötting <emil.oetting@gmx.de>
2025-02-13 04:21:46 +0100

    Added script for generating index.rst required for building docs via Sphinx

---
+++ docs/master_index_builder.sh
@@ -0,0 +1,9 @@
+echo "Welcome to My Project's Documentation" > docs/index.rst
+echo "======================================" >> docs/index.rst
+echo "" >> docs/index.rst
+echo ".. toctree::" >> docs/index.rst
+echo "   :maxdepth: 2" >> docs/index.rst
+echo "   :caption: Contents:" >> docs/index.rst
+echo "" >> docs/index.rst
+echo "   modules/index" >> docs/index.rst
+echo "index.rst created"
