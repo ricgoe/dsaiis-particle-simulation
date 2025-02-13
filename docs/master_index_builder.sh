7fbd7c196d92d7f1c04c19f7640f4d60fb0eb555
Emil Ötting <emil.oetting@gmx.de>
2025-02-13 04:35:01 +0100

    Rebuild script for better readability

--- docs/master_index_builder.sh
+++ docs/master_index_builder.sh
@@ -1,9 +1,13 @@
-echo "Welcome to the DSAIIS Particle Project Documentation" > docs/index.rst
-echo "======================================" >> docs/index.rst
-echo "" >> docs/index.rst
-echo ".. toctree::" >> docs/index.rst
-echo "   :maxdepth: 2" >> docs/index.rst
-echo "   :caption: Contents:" >> docs/index.rst
-echo "" >> docs/index.rst
-echo "   modules/index" >> docs/index.rst
-echo "index.rst created"
+echo "Creating index.rst for Sphinx documentation builder"
+echo << EOF > index.rst
+
+Welcome to DSAIIS Particle Simulation's documentation
+=====================================================
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+   
+   index
+EOF
+
+echo "Successfully created index.rst"
