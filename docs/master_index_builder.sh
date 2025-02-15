a038d1c7831df0fc9ca88be6b11c5f7d3c955eef
Emil Ötting <emil.oetting@gmx.de>
2025-02-16 00:28:34 +0100

    Increse maxdepth

--- docs/master_index_builder.sh
+++ docs/master_index_builder.sh
@@ -5,7 +5,7 @@ cat << EOF > ./index.rst
 Welcome to DSAIIS Particle Simulation's documentation
 =====================================================
 .. toctree::
-   :maxdepth: 4
+   :maxdepth: 6
    :caption: Contents:
 
    modules
