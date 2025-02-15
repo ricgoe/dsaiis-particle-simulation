9d96e6360725032f3aead210487059abde4bbaa4
Emil Ötting <emil.oetting@gmx.de>
2025-02-15 11:56:58 +0100

    Debug content variable

--- docs/master_index_builder.sh
+++ docs/master_index_builder.sh
@@ -1,17 +1,17 @@
 SRC_DIR="./api"
-content=""
+content="   checker"
 
-echo "Collecting module-rst files"
-for file in "$SRC_DIR"/*.rst; do
-    if [ -f "$file" ]; then
-        if [ "$(basename "$file")" = "index.rst" ]; then
-            continue
-        fi
-        filename=$(basename "$file" .rst)
-        echo "Adding $filename to index.rst"
-        content+=$'   '"${filename}"$'\n'
-    fi
-done
+#echo "Collecting module-rst files"
+#for file in "$SRC_DIR"/*.rst; do
+#    if [ -f "$file" ]; then
+#        if [ "$(basename "$file")" = "index.rst" ]; then
+#            continue
+#        fi
+#        filename=$(basename "$file" .rst)
+#        echo "Adding $filename to index.rst"
+#        content+=$'   '"${filename}"$'\n'
+#    fi
+#done
 
 echo "Creating index.rst for Sphinx documentation builder"
 cat << EOF > ./index.rst
