2e0c505d6f4eae0638e06d91dc0690cab2d52b45
Emil Ötting <emil.oetting@gmx.de>
2025-02-15 13:31:07 +0100

    Change path apidoc

--- docs/master_index_builder.sh
+++ docs/master_index_builder.sh
@@ -1,17 +1,3 @@
-SRC_DIR="./api"
-content=""
-
-echo "Collecting module-rst files"
-for file in "$SRC_DIR"/*.rst; do
-    if [ -f "$file" ]; then
-        #if [ "$(basename "$file")" = "index.rst" ]; then
-        #    continue
-        #fi
-        filename=$(basename "$file" .rst)
-        echo "Adding $filename to index.rst"
-        content+=$'   '"${filename}"$'\n'
-    fi
-done
 
 echo "Creating index.rst for Sphinx documentation builder"
 cat << EOF > ./index.rst
@@ -21,8 +7,8 @@ Welcome to DSAIIS Particle Simulation's documentation
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
-   
-${content}
+
+   modules
    
 EOF
 
