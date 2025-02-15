b8fea1d09312b216af426d4b3aff9970e04fae22
Emil Ötting <emil.oetting@gmx.de>
2025-02-15 12:08:08 +0100

    Reset master index changes

--- docs/master_index_builder.sh
+++ docs/master_index_builder.sh
@@ -1,17 +1,17 @@
 SRC_DIR="./api"
-content="   checker"
+content=""
 
-#echo "Collecting module-rst files"
-#for file in "$SRC_DIR"/*.rst; do
-#    if [ -f "$file" ]; then
-#        if [ "$(basename "$file")" = "index.rst" ]; then
-#            continue
-#        fi
-#        filename=$(basename "$file" .rst)
-#        echo "Adding $filename to index.rst"
-#        content+=$'   '"${filename}"$'\n'
-#    fi
-#done
+echo "Collecting module-rst files"
+for file in "$SRC_DIR"/*.rst; do
+    if [ -f "$file" ]; then
+        if [ "$(basename "$file")" = "index.rst" ]; then
+            continue
+        fi
+        filename=$(basename "$file" .rst)
+        echo "Adding $filename to index.rst"
+        content+=$'   '"${filename}"$'\n'
+    fi
+done
 
 echo "Creating index.rst for Sphinx documentation builder"
 cat << EOF > ./index.rst
@@ -22,8 +22,7 @@ Welcome to DSAIIS Particle Simulation's documentation
    :maxdepth: 2
    :caption: Contents:
 
-   #api/index
-   test.md
+   api/index
 ${content}
    
 EOF
